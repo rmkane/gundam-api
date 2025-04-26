ee90ddfc867468c93a6d8f4883f3e007849d1dd1
rmkane <rmkane@proton.me>
2025-04-25 23:11:28 -0400

    Improve Open API query params

--- server/node/src/schemas/request/query.ts
+++ server/node/src/schemas/request/query.ts
@@ -1,5 +1,35 @@
 import { z } from 'zod'
 
-export const BooleanParamSchema = z.enum(['true', 'false']).transform((v) => v === 'true')
+/**
+ * A boolean parameter that accepts 'true'/'false' strings from HTTP queries,
+ * but appears as a boolean in the OpenAPI schema.
+ *
+ * @example
+ * // In HTTP query: ?includeDeleted=true
+ * // In code: includeDeleted === true
+ *
+ * // In HTTP query: ?includeDeleted=false
+ * // In code: includeDeleted === false
+ */
+export const BooleanParamSchema = z
+  .string()
+  .transform((v) => v === 'true')
+  .describe('Boolean parameter')
+  .openapi({ type: 'boolean' })
 
-export const NumberParamSchema = z.string().transform(Number)
+/**
+ * A number parameter that accepts numeric strings from HTTP queries,
+ * but appears as a number in the OpenAPI schema.
+ *
+ * @example
+ * // In HTTP query: ?seriesId=1
+ * // In code: seriesId === 1
+ *
+ * // In HTTP query: ?page=2
+ * // In code: page === 2
+ */
+export const NumberParamSchema = z
+  .string()
+  .transform(Number)
+  .describe('Number parameter')
+  .openapi({ type: 'number' })
