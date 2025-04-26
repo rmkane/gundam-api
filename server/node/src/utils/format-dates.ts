3d55d6cadffc6051a68753be113c251f8da8873b
rmkane <rmkane@proton.me>
2025-04-26 10:55:47 -0400

    Housekeeping

--- server/node/src/utils/format-dates.ts
+++ server/node/src/utils/format-dates.ts
@@ -1,12 +1,14 @@
 import { TimestampFields } from '../db/types.js'
 
-export const formatDates = <T extends TimestampFields>(
-  item: T
-): Omit<T, keyof TimestampFields> & {
+interface FormattedTimestampFields {
   createdAt: string
   updatedAt: string
   deletedAt: string | null
-} => ({
+}
+
+export const formatDates = <T extends TimestampFields>(
+  item: T
+): Omit<T, keyof TimestampFields> & FormattedTimestampFields => ({
   ...item,
   createdAt: item.createdAt?.toISOString() ?? new Date().toISOString(),
   updatedAt: item.updatedAt?.toISOString() ?? new Date().toISOString(),
